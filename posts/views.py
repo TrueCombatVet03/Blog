74b3927d5beded925021e39402e3869d0d0c34b1
Kevin <armyveteranoif247@gmail.com>
2022-05-06 19:52:15 -0500

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,8 @@ from django.views.generic.edit import (
     UpdateView,
     DeleteView,
 )
-from .models import Post 
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin 
 from django.urls import reverse_lazy
 
 
@@ -17,18 +18,18 @@ class PostDetailView(DetailView):
     template_name = 'posts/detail.html'
     model = Post 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
         template_name = 'posts/new.html'
         model = Post 
         fields = ['title', 'author','body']
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'posts/edit.html'
     model = Post
     fields = ['title', 'body']
 
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     template_name = 'posts/delete.html'
     model = Post
     success_url = reverse_lazy('post_list')
